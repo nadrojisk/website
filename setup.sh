834c4c8d8f52eeac8ab32119fd401d3c2366cc41
nadrojisk <jjs0029@auburn.edu>
2021-03-29 17:16:25 -0500

    Swapped out bash script for docker-compose

--- setup.sh
+++
@@ -1,2 +0,0 @@
-sudo docker run --rm --volume="$PWD:/srv/jekyll" -it jekyll/jekyll jekyll build
-sudo docker run --name myblog --volume="$PWD:/srv/jekyll" -p 4000:4000 -it jekyll/jekyll jekyll serve --watch
